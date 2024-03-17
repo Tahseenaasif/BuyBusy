7783bbc5c21a1feb00633c847d0bd313d94efb80
Tahseenaasif <94477366+Tahseenaasif@users.noreply.github.com>
2024-03-17 23:59:03 +0530

    Update firebaseinit.js

--- src/firebaseinit.js
+++ src/firebaseinit.js
@@ -3,18 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCt24p7U5_-Sfuj1BSCXtCxM747l9BNr0Y",
-
-  authDomain: "buybusy-da290.firebaseapp.com",
-
-  projectId: "buybusy-da290",
-
-  storageBucket: "buybusy-da290.appspot.com",
-
-  messagingSenderId: "440805383340",
-
-  appId: "1:440805383340:web:5e57e61c929ef4a8355309"
-
+  //add your firebase config here 
 };
 
 
